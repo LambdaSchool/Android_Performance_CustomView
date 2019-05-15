6a864ca42997eeedcd57be9c28c8654561929eb9
Shoonsk8 <shoon@att.net>
2019-05-15 07:52:23 -0400

    Original dial view

--- app/src/main/java/com/shoon/android_performance_customview/MainActivity.java
+++ app/src/main/java/com/shoon/android_performance_customview/MainActivity.java
@@ -1,13 +1,15 @@
 package com.shoon.android_performance_customview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
-    }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final DialView gv=findViewById(R.id.dial_volume);
+        //SliderView sl=findViewById(R.id.slider_rate);
+   }
 }
